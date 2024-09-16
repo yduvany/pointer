df314aeeb1b4c77580221bb58f3f30096ec8d30c
User <User@DESKTOP-6U5V4IP>
2024-09-16 19:01:54 +0300

    Add project files.

---
+++ task 1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, * ora_a, * ora_b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	ora_a = &a;
+	ora_b = &b;
+	if (*ora_a > *ora_b) {
+		cout << *ora_a << " is bigger number" << endl;
+	}
+	else {
+		cout << *ora_b << " is bigger number" << endl;
+	}
+	return 0;
+}
